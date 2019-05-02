d4dce23fbf0945b723b6c4ea534883d494113e5f
Filipe Fusco <filipe.fusco@gmail.com>
2019-05-02 12:01:59 -0300

    handlers basic structure and connection with mongo

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main(){
-	
+import (
+	"net/http"
+
+	"github.com/fusco2k/go-microservice-user-di/config"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Env struct {
+	cl *mongo.Collection
+}
+
+func main() {
+	cl := config.NewSession("mongodb://localhost:27017", "users", "catalog")
+
+	env := Env{cl}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		http.Redirect(w,r,"/users", 307)
+	})
+	http.HandleFunc("users", func(w http.ResponseWriter, r *http.Request){
+		switch r.Method{
+		case "GET":
+			
+		case "POST":
+
+		case "PUT":
+
+		case "DELETE":
+			
+		}
+	})
+	http.ListenAndServe("8080", nil)
 }
