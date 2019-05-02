d7d67ea380c17ddf094ed904a01ba8c80e7f677c
Filipe Fusco <filipe.fusco@gmail.com>
2019-05-02 12:00:58 -0300

    wire up mongo connection

--- config/db.go
+++ config/db.go
@@ -1,2 +1,34 @@
 package config
 
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+//NewSession creates a new mongo session returning a pointer to the asked database collection
+func NewSession(uri, db, cl string) *mongo.Collection {
+
+	//create a context for comunicate with mongodb
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	//iniatiate the pointed client and connects to the mongo server
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("uri"))
+	if err != nil {
+		cancel()
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	//pings the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		cancel()
+		log.Fatal(err)
+	}
+	//assign a pointes collection
+	col := client.Database("db").Collection("cl")
+
+	return col
+}
