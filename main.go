a8c77dc96212f6dc01a9405de3ae2b5eed56e0ca
Filipe Fusco <filipe.fusco@gmail.com>
2019-05-18 03:21:38 -0300

    rewrited using httprouter and wireup the DI

--- main.go
+++ main.go
@@ -1,37 +1,28 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/fusco2k/go-microservice-user-di/config"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/fusco2k/go-microservice-user-di/users"
+	"github.com/julienschmidt/httprouter"
 )
-//Env contains dependencys to inject
-type Env struct {
-	cl *mongo.Collection
-}
 
 func main() {
-	cl := config.NewSession("mongodb://localhost:27017", "users", "catalog")
-
-	env := Env{cl}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		http.Redirect(w,r,"/users", 307)
-	})
-	http.HandleFunc("/api/users", func(w http.ResponseWriter, r *http.Request){
-		switch r.Method{
-		case "GET":
-		case "POST":
-		case "PUT":
-		case "DELETE":
-		}
-	})
-	http.HandleFunc("/api/users/", func(w http.ResponseWriter, r *http.Request){
-		switch r.Method{
-		case "GET":
-		}
-	})
-	http.ListenAndServe("8080", nil)
+	//mux
+	router := httprouter.New()
+	//dependency config
+	cl := config.NewSession("mongodb://localhost:27017")
+	defer cl.Disconnect(context.Background())
+	env := &config.Env{CL: cl.Database("users").Collection("catalog")}
+	//routes
+	router.GET("/api/users", users.UserIndex(env))
+	// router.POST("/api/users")
+	// router.PUT("/api/users")
+	// router.DELETE("/api/users")
+	// router.GET("/api/users/:id", users.UserIndex(env))
+	//server at port 8080 using httprouter as router
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
